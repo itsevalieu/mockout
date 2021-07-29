8a0426358c55fb32af5a0dd07bb08f14cdd4cbcc
Eva Lieu <itsevalieu@gmail.com>
2021-07-29 00:54:13 -0700

    Fixed async helper functions with promises, filterFlashcardsByTopics now returns the correct flashcards based on topics; organized component folders for Hero, Item, and ItemList; slight styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
 import { useState, useEffect, useRef } from "react";
-import { Map } from "typescript";
 import './App.scss';
-import Hero from "./components/Hero";
-import Item, { ItemProps } from "./components/Item";
+import Hero from "./components/Hero/Hero";
+import { ItemProps } from "./components/ItemList/Item";
+import ItemList from "./components/ItemList/ItemList";
 const sw = require("stopword");
 
 const App = () => {
-  const availableTopics = [
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const api: string = process.env.REACT_APP_API_URL || "";
+  const topics = [
     "javascript",
     "react",
     "frontend",
+    "css",
+    "node",
+    // "nodejs"
     // "frameworks",
     // "github",
     // "redux",
     // "es6",
-    "css",
   ];
   const [filteredTopics, setFilteredTopics] = useState<string[]>([]);
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-  const api: string = process.env.REACT_APP_API_URL || "";
-  // const options = {
-  //     method: 'GET', // *GET, POST, PUT, DELETE, etc.
-  //     headers: {
-  //       'Content-Type': 'text/plain'
-  //       // 'Content-Type': 'application/x-www-form-urlencoded',
-  //     }
-  // };
   const [items, setItems] = useState<any[]>([]);
+  const [fetchedItems, setFetchedItems] = useState<any[]>([]);
   const [isLoading, setLoading] = useState<boolean>(false);
   useEffect(() => {
-    console.log("UseEffect");
+    console.log("UseEffect, Fetch all the github issues");
     const fetchFlashcards = () => {
       console.log(api);
       fetch(api).then((res) => {
@@ -38,74 +34,54 @@ const App = () => {
         let flashcards: any[] = [];
         for(let item of items) {
           if(item.labels.length === 0) continue;
-          let flashcard: ItemProps = {
-            question: item.title,
-            answer: item.body,
-            categories: [...item.labels.map((label: any) => label.name)],
-            link: []
+          let categories: string[] = [...item.labels.map((label: any) => label.name)].sort();
+          if(categories.includes("flashcard")) {
+            let flashcard: ItemProps = {
+              question: item.title,
+              answer: item.body,
+              categories: categories,
+              link: []
+            }
+            flashcards.push(flashcard);
+          } else {
+            continue;
           }
-          flashcards.push(flashcard);
         }
-        console.log("XXX. Fetched all clean flashcards", flashcards);
-        let filteredFlashcards = filterCategoriesByFlashcard("flashcard", flashcards);
-        setItems(filteredFlashcards);
+        console.log("1. Fetched all clean flashcards", flashcards);
+        // let filteredFlashcards = filterCategoriesByFlashcard("flashcard", flashcards);
+        setFetchedItems(flashcards);
       });
     }
     fetchFlashcards();
     return () => console.log("Unmount");
   }, [api]); 
-  
-  const filteredFlashcardsByTopics = (items: any[]) => {
-    let newItems = items.filter((item, index) => {
-      console.log("5. filter flashcards by categories if it includes the topics, items:", items);
-      console.log("6. filteredTopics", filteredTopics);
-      for(let topic of filteredTopics) {
-        console.log("7. topic in category,", index, topic, item.categories);
-        return item.categories.includes(topic);
-      }
-    });
 
-    setItems(newItems);
-    console.log("Newly filtered", newItems);
-  }
   const handleFormSubmit = async (event: any) => {
     event.preventDefault();
-    console.log("Submit form start");
+    console.log("2. Submit form start");
+    setLoading(true);
     let textNode = textAreaRef.current || null;
     if(textNode && textNode.value) {
-      setLoading(true);
-      let textArray: string[] = cleanText(textNode.value);
-      textArray = removeStopWords(textArray);
-      let hashedTextArray = hashWords(textArray);
-      console.log("1. hashedTextArray", hashedTextArray);
-      await detectTopics(hashedTextArray, availableTopics);
-      console.log("3. detectopics stopped?");
-      console.log("4. ===before filtered flashcards by topic", filteredTopics);
-  
-      await filteredFlashcardsByTopics(items);
+      let cleanedTextArray: string[] = cleanTextInput(textNode.value);
+      let hashedMapWords = hashMapWords(cleanedTextArray);
+      let newTopics: string[] = await detectTopics(hashedMapWords, topics);
+      await filterFlashcardsByTopics(fetchedItems, newTopics);
       setLoading(false);
-
       textNode.value = "";
       textNode.focus();
     } else {
       console.log("No job description submitted.");
     }
   }
-  //detectTopics: compare hashMap keys with flashcardtopics
-  const detectTopics = (hashedTextArray: any, availableTopics: string[]) => {
-    let filteredTopics = availableTopics.filter(topic => {
-      return hashedTextArray.has(topic);
-    });
-    setFilteredTopics(filteredTopics);
-    console.log("2. detectTopics filteredTopics", filteredTopics);
-  }
   /**
    * Cleans textArea input by changing the string to lowercase, then splitting by " "
    * @param text 
    * @returns string[]
    */
-  const cleanText = (text: string) => {
-    return text.trim().toLowerCase().replace(/[^\w\s]|_/g, "").split(" ").sort();
+  const cleanTextInput = (text: string) => {
+    console.log("3. cleanTextInput");
+    let cleanedWords = text.trim().toLowerCase().replace(/[^\w\s]|_/g, "").split(" ").sort();
+    return removeStopWords(cleanedWords);
   }
   /**
    * Removes stop words like an, a, the, etc from the textArray 
@@ -120,7 +96,8 @@ const App = () => {
    * @param textArray 
    * @returns hashmap
    */
-  const hashWords = (textArray: string[]) => {
+  const hashMapWords = (textArray: string[]) => {
+    console.log("4. hashMapWords");
     const hashArray = new Map();
     for(let word of textArray) {
       if(hashArray.has(word)) {
@@ -129,8 +106,37 @@ const App = () => {
         hashArray.set(word, 1);
       }
     }
+    console.log("4a. hashArray", hashArray);
     return hashArray;
   }
+
+  //detectTopics: compare hashMap keys with flashcardtopics
+  const detectTopics = (hashedTextArray: any, topics: string[]) => {
+    console.log("5. detectTopics");
+    const p = new Promise<string[]>(async (resolve, reject) => {
+      let cleanedTopics: string[] = await topics.filter(topic => hashedTextArray.has(topic));
+      setFilteredTopics(cleanedTopics);
+      console.log("5a filteredTopics can't be empty", cleanedTopics);
+      resolve(cleanedTopics);
+    });
+    return p;
+  }
+  
+  //todo
+  const filterFlashcardsByTopics = (items: any[], topics: string[]) => {
+    console.log("6. filteredFlashcardsByTopics", items, topics);
+    let newItems = items.filter((item, index) => {
+      console.log("6a. item: ", index, item.categories); 
+      return topics.some(topic => {
+        console.log("6a. topic: ", topic);
+        return item.categories.includes(topic);
+      });
+    });
+    setItems(newItems);
+    console.log("Newly filtered", newItems);
+  }
+  
+  
   /**
    * Filters categories and returns a list of items with the chosen category
    * @param category 
@@ -155,22 +161,8 @@ const App = () => {
             <textarea ref={textAreaRef} placeholder="Insert Job Description text" autoFocus/>
             <button type="submit">Generate</button>
           </form>
-          { isLoading ? 
-            <div><p>Loading...</p></div> : 
-            <div>
-              <div className="description">
-                <h3>Practice Interview Questions</h3>
-                <p>Based on the pasted job description, here are some practice interview questions that were generated below:</p>
-                <h4>Topics Include:</h4>
-                <div id="categories">{ filteredTopics.length === 0 ?  <span>None</span> : filteredTopics.map((topic, index) => <span key={index}>{topic}</span>) }</div>
-                <br/>
-              </div>
-              <div className="ItemList">
-                <ul>
-                  { items.map((item, index) => <Item key={index} {...item}/>) }
-                </ul>
-              </div>
-            </div>
+          {
+            isLoading ? <div><p>Loading...</p></div> : <ItemList filteredTopics={filteredTopics} items={items}/>
           }
         </div>
       </main>
